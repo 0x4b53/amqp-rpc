df2e3a103131ef4941f7dae7b3a6aa0afc788871
Simon Sawert <simon@sawert.se>
2018-04-12 22:33:43 +0200

    Support to add TLS config, be quiet in tests

--- server/server_test.go
+++ server/server_test.go
@@ -2,14 +2,25 @@ package server
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net"
 	"testing"
 	"time"
 
+	"github.com/bombsimon/amqp-rpc/logger"
 	"github.com/streadway/amqp"
 	. "gopkg.in/go-playground/assert.v1"
 )
+
+func noEcho() {
+	silentLogger := log.New(ioutil.Discard, "", log.LstdFlags)
+
+	logger.SetInfoLogger(silentLogger)
+	logger.SetWarnLogger(silentLogger)
+}
 
 func replyToConsumer(t *testing.T, ch *amqp.Channel) (<-chan amqp.Delivery, string) {
 	q, err := ch.QueueDeclare(
@@ -36,7 +47,12 @@ func replyToConsumer(t *testing.T, ch *amqp.Channel) (<-chan amqp.Delivery, stri
 }
 
 func TestPublishReply(t *testing.T) {
+	noEcho()
+
 	s := New()
+	s.SetTLSConfig(new(tls.Config))
+
+	NotEqual(t, s.dialconfig.TLSClientConfig, nil)
 
 	s.AddHandler("myqueue", func(ctx context.Context, d *amqp.Delivery) []byte {
 		return []byte(fmt.Sprintf("Got message: %s", d.Body))
@@ -97,6 +113,8 @@ func testDialer(t *testing.T) (func(string, string) (net.Conn, error), func() ne
 }
 
 func TestReconnect(t *testing.T) {
+	noEcho()
+
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 
 	Equal(t, err, nil)
