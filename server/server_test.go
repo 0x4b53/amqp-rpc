856547de63f3e60f8ad753913885f8817f41b117
Simon Sawert <simon@sawert.se>
2018-04-20 23:58:21 +0200

    Use RPC client in server tests

--- server/server_test.go
+++ server/server_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/bombsimon/amqp-rpc/client"
 	"github.com/bombsimon/amqp-rpc/logger"
 	"github.com/streadway/amqp"
 	. "gopkg.in/go-playground/assert.v1"
@@ -20,30 +21,6 @@ func noEcho() {
 
 	logger.SetInfoLogger(silentLogger)
 	logger.SetWarnLogger(silentLogger)
-}
-
-func replyToConsumer(t *testing.T, ch *amqp.Channel) (<-chan amqp.Delivery, string) {
-	q, err := ch.QueueDeclare(
-		"",    // name
-		false, // durable
-		true,  // delete when unused
-		true,  // exclusive
-		false, // noWait
-		nil,   // arguments
-	)
-	Equal(t, err, nil)
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	Equal(t, err, nil)
-	return msgs, q.Name
 }
 
 func TestPublishReply(t *testing.T) {
@@ -60,31 +37,11 @@ func TestPublishReply(t *testing.T) {
 
 	go s.ListenAndServe("amqp://guest:guest@localhost:5672/")
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	Equal(t, err, nil)
-	defer conn.Close()
+	client := client.New("amqp://guest:guest@localhost:5672/")
+	reply, err := client.Publish("myqueue", []byte("this is a message"), true)
 
-	ch, err := conn.Channel()
-	Equal(t, err, nil)
-	defer ch.Close()
-
-	msgs, replyQueue := replyToConsumer(t, ch)
-
-	err = ch.Publish(
-		"",        // exchange
-		"myqueue", // routing key
-		false,     // mandatory
-		false,     // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			ReplyTo:     replyQueue,
-			Body:        []byte("this is a message"),
-		},
-	)
 	Equal(t, err, nil)
 
-	reply, ok := <-msgs
-	Equal(t, ok, true)
 	Equal(t, reply.Body, []byte("Got message: this is a message"))
 }
 
@@ -131,44 +88,30 @@ func TestReconnect(t *testing.T) {
 		false, // noWait
 	)
 
-	msgs, replyQueue := replyToConsumer(t, ch)
-
 	s := New()
 	dialer, getNetConn := testDialer(t)
 	s.SetAMQPConfig(amqp.Config{
 		Dial: dialer,
 	})
+
 	s.AddHandler("myqueue", func(ctx context.Context, d *amqp.Delivery) []byte {
 		return []byte(fmt.Sprintf("Got message: %s", d.Body))
 	})
+
 	go s.ListenAndServe("amqp://guest:guest@localhost:5672/")
 
 	// Sleep a bit to ensure server is started.
 	time.Sleep(50 * time.Millisecond)
+	client := client.New("amqp://guest:guest@localhost:5672/")
 
 	for i := 0; i < 2; i++ {
-		message := fmt.Sprintf("this is message %v", i)
-		err = ch.Publish(
-			"",        // exchange
-			"myqueue", // routing key
-			false,     // mandatory
-			false,     // immediate
-			amqp.Publishing{
-				ContentType: "text/plain",
-				ReplyTo:     replyQueue,
-				Body:        []byte(message),
-			},
-		)
+		message := []byte(fmt.Sprintf("this is message %v", i))
+		reply, err := client.Publish("myqueue", message, true)
+
 		Equal(t, err, nil)
 
 		getNetConn().Close()
 
-		select {
-		case reply, ok := <-msgs:
-			Equal(t, ok, true)
-			Equal(t, reply.Body, []byte(fmt.Sprintf("Got message: %s", message)))
-		case <-time.After(1100 * time.Millisecond):
-			t.Error("reply did not arrive")
-		}
+		Equal(t, reply.Body, []byte(fmt.Sprintf("Got message: %s", message)))
 	}
 }
