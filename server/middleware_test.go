02fbac132ce5efacf9ac0a2b368bbcd8e8e5dbb1
Andreas Karlsson <andreas.karlsson87@gmail.com>
2018-06-18 19:41:08 +0200

    Add test for middleware chain

---
+++ server/middleware_test.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/streadway/amqp"
+	. "gopkg.in/go-playground/assert.v1"
+)
+
+func traceMiddleware(ID int) func(HandlerFunc) HandlerFunc {
+	return func(next HandlerFunc) HandlerFunc {
+		return func(ctx context.Context, rw *ResponseWriter, d amqp.Delivery) {
+			fmt.Fprint(rw, ID)
+			next(ctx, rw, d)
+			fmt.Fprint(rw, ID)
+		}
+	}
+}
+
+func TestMiddlewareChain(t *testing.T) {
+	handler := Middlewares(
+		func(ctx context.Context, rw *ResponseWriter, d amqp.Delivery) {
+			fmt.Fprint(rw, "X")
+		},
+		traceMiddleware(1),
+		traceMiddleware(2),
+		traceMiddleware(3),
+		traceMiddleware(4),
+	)
+
+	rWriter := &ResponseWriter{
+		publishing: &amqp.Publishing{},
+	}
+
+	handler(context.Background(), rWriter, amqp.Delivery{})
+
+	Equal(t, string(rWriter.Publishing().Body), "1234X4321")
+}
