d57392307041b553c2e21d36b53a0c54d0430a7a
Andreas Karlsson <andreas.karlsson87@gmail.com>
2018-07-11 00:08:37 +0200

    Graceful shutdown of server

--- examples/server/main.go
+++ examples/server/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/bombsimon/amqp-rpc/server"
 
@@ -14,10 +17,22 @@ func main() {
 	s := server.New("amqp://guest:guest@localhost:5672/")
 
 	s.Bind(server.DirectBinding("upper", upper))
+	s.Bind(server.DirectBinding("beat", beat))
+
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		<-sigs
+		s.Stop()
+	}()
 
 	s.ListenAndServe()
 }
 
 func upper(c context.Context, rw *server.ResponseWriter, d amqp.Delivery) {
 	fmt.Fprint(rw, strings.ToUpper(string(d.Body)))
+}
+
+func beat(c context.Context, rw *server.ResponseWriter, d amqp.Delivery) {
+	fmt.Fprintf(rw, "beat")
 }
