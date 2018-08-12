786e4de26fd29c35f45ef066fa51709b01b7096e
Simon Sawert <simon@sawert.se>
2018-08-12 20:37:32 +0200

    Implement ack aware acknowledger to support acknowledge outside server. Closes #27

---
+++ acknowledger.go
@@ -0,0 +1,35 @@
+package amqprpc
+
+import (
+	"github.com/streadway/amqp"
+)
+
+// ackAwareChannel implements the amqp.Acknowledger interface with the addtion
+// that it can tell if a message has been acked, nacked or rejected.
+type ackAwareChannel struct {
+	ch      amqp.Acknowledger
+	handled bool
+}
+
+func (a *ackAwareChannel) Ack(tag uint64, multiple bool) error {
+	a.handled = true
+
+	return a.ch.Ack(tag, multiple)
+}
+
+func (a *ackAwareChannel) Nack(tag uint64, multiple bool, requeue bool) error {
+	a.handled = true
+
+	return a.ch.Nack(tag, multiple, requeue)
+}
+
+func (a *ackAwareChannel) Reject(tag uint64, requeue bool) error {
+	a.handled = true
+
+	return a.ch.Reject(tag, requeue)
+}
+
+// IsHandled returns a boolean value telling if the acknowledger has been called.
+func (a *ackAwareChannel) IsHandled() bool {
+	return a.handled
+}
