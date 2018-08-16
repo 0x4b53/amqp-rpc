14be398982e4672c963328aa59c577370c5d585f
Simon Sawert <simon@sawert.se>
2018-08-16 14:42:13 +0200

    Add support for graceful shutdown to client. Closes #22

--- connection.go
+++ connection.go
@@ -1,7 +1,20 @@
 package amqprpc
 
 import (
+	"errors"
+
 	"github.com/streadway/amqp"
+)
+
+var (
+	// ErrUnexpectedConnClosed is returned by ListenAndServe() if the server
+	// shuts down without calling Stop() and if AMQP does not give an error
+	// when said shutdown happens.
+	ErrUnexpectedConnClosed = errors.New("unexpected connection close without specific error")
+
+	// ErrTimeout is an error returned when a client request does not
+	// receive a response within the client timeout duration.
+	ErrTimeout = errors.New("request timed out")
 )
 
 // ExchangeDeclareSettings is the settings that will be used when a handler
@@ -41,4 +54,29 @@ type ConsumeSettings struct {
 type PublishSettings struct {
 	Mandatory bool
 	Immediate bool
+}
+
+func monitorChannels(stopChan chan struct{}, connectionChannels []chan *amqp.Error) error {
+	result := make(chan error)
+
+	// Setup monitoring for connection channels, can be several connections.
+	// The first one closed will yield the error.
+	for _, c := range connectionChannels {
+		go func(c chan *amqp.Error) {
+			err, ok := <-c
+			if !ok {
+				result <- ErrUnexpectedConnClosed
+			}
+
+			result <- err
+		}(c)
+	}
+
+	// Setup monitoring for application channel
+	go func() {
+		<-stopChan
+		result <- nil
+	}()
+
+	return <-result
 }
