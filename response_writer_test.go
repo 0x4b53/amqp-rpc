814b7062663f115cd9af6211b13535897b664630
Simon Sawert <simon@sawert.se>
2019-08-22 15:41:42 +0200

    Make request and response (writer) interface the same (public fields)

--- response_writer_test.go
+++ response_writer_test.go
@@ -12,36 +12,36 @@ func TestResponseWriter(t *testing.T) {
 	assert := assert.New(t)
 
 	rw := &ResponseWriter{
-		publishing: &amqp.Publishing{},
+		Publishing: &amqp.Publishing{},
 	}
 
-	assert.Equal(false, rw.immediate, "immediate starts false")
-	assert.Equal(false, rw.mandatory, "mandatory starts false")
+	assert.Equal(false, rw.Immediate, "immediate starts false")
+	assert.Equal(false, rw.Mandatory, "mandatory starts false")
 
-	rw.Immediate(true)
-	rw.Mandatory(true)
+	rw.Immediate = true
+	rw.Mandatory = true
 
-	assert.Equal(true, rw.immediate, "immediate is changed to true")
-	assert.Equal(true, rw.mandatory, "mandatory is changed to true")
+	assert.Equal(true, rw.Immediate, "immediate is changed to true")
+	assert.Equal(true, rw.Mandatory, "mandatory is changed to true")
 
-	rw.Immediate(false)
-	rw.Mandatory(false)
+	rw.Immediate = false
+	rw.Mandatory = false
 
-	assert.Equal(false, rw.immediate, "immediate are changed to false")
-	assert.Equal(false, rw.mandatory, "mandatory is changed to false")
+	assert.Equal(false, rw.Immediate, "immediate are changed to false")
+	assert.Equal(false, rw.Mandatory, "mandatory is changed to false")
 
 	fmt.Fprint(rw, "Foo")
-	assert.Equal([]byte("Foo"), rw.Publishing().Body, "writing to response writer is reflected in the body")
+	assert.Equal([]byte("Foo"), rw.Publishing.Body, "writing to response writer is reflected in the body")
 
 	fmt.Fprint(rw, "Bar")
-	assert.Equal([]byte("FooBar"), rw.Publishing().Body, "writing to response writer multiple times is reflected in the body")
+	assert.Equal([]byte("FooBar"), rw.Publishing.Body, "writing to response writer multiple times is reflected in the body")
 
 	rw.WriteHeader("some-header", "writing")
-	assert.Equal("writing", rw.Publishing().Headers["some-header"], "writing headers will set the headers of the publishing")
+	assert.Equal("writing", rw.Publishing.Headers["some-header"], "writing headers will set the headers of the publishing")
 
 	rw.WriteHeader("some-header", "writing-again")
-	assert.Equal("writing-again", rw.Publishing().Headers["some-header"], "overwriting headers will set the headers of the publishing")
+	assert.Equal("writing-again", rw.Publishing.Headers["some-header"], "overwriting headers will set the headers of the publishing")
 
 	rw.WriteHeader("some-header", 1)
-	assert.Equal(1, rw.Publishing().Headers["some-header"], "writing other types than s t rings to header works")
+	assert.Equal(1, rw.Publishing.Headers["some-header"], "writing other types than s t rings to header works")
 }
