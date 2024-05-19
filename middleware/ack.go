0fb00d3f17f6e1c838f2bc0e1230affb186696c1
Simon Sawert <simon@sawert.se>
2024-05-19 10:41:03 +0200

    Make names clearer
    Ensure the names of the build-in `OnErrFunc` is clear in its naming so
    it's obvious they belong to the `AckDelivery` middleware.

--- middleware/ack.go
+++ middleware/ack.go
@@ -11,22 +11,22 @@ import (
 // error from `Ack`. The error and the delivery will be passed.
 type OnErrFunc func(err error, delivery amqp.Delivery)
 
-// OnErrLogError is a built-in function that will log the error if any is
+// OnAckErrorLog is a built-in function that will log the error if any is
 // returned from `Ack`.
 //
-//	middleware := AckDelivery(OnErrLogError(log.Printf))
-func OnErrLogError(logFn amqprpc.LogFunc) OnErrFunc {
+//	middleware := AckDelivery(OnAckErrorLog(log.Printf))
+func OnAckErrorLog(logFn amqprpc.LogFunc) OnErrFunc {
 	return func(err error, delivery amqp.Delivery) {
 		logFn("could not ack delivery (%s): %v\n", delivery.CorrelationId, err)
 	}
 }
 
-// OnErrSendOnChannel will first log the error and correlation ID and then try
-// to send on the passed channel. If no one is consuming on the passed channel
-// the middleware will not block but instead log a message about missing channel
-// consumers.
-func OnErrSendOnChannel(logFn amqprpc.LogFunc, ch chan struct{}) OnErrFunc {
-	logErr := OnErrLogError(logFn)
+// OnAckErrorSendOnChannel will first log the error and correlation ID and then
+// try to send on the passed channel. If no one is consuming on the passed
+// channel the middleware will not block but instead log a message about missing
+// channel consumers.
+func OnAckErrorSendOnChannel(logFn amqprpc.LogFunc, ch chan struct{}) OnErrFunc {
+	logErr := OnAckErrorLog(logFn)
 
 	return func(err error, delivery amqp.Delivery) {
 		logErr(err, delivery)
@@ -40,8 +40,8 @@ func OnErrSendOnChannel(logFn amqprpc.LogFunc, ch chan struct{}) OnErrFunc {
 }
 
 // AckDelivery is a middleware that will acknowledge the delivery after the
-// handler has been executed. Any error returned from d.Ack will be passed
-// to the provided logFunc.
+// handler has been executed. If the Ack fails the error and the `amqp.Delivery`
+// will be passed to the `OnErrFunc`.
 func AckDelivery(onErrFn OnErrFunc) amqprpc.ServerMiddlewareFunc {
 	return func(next amqprpc.HandlerFunc) amqprpc.HandlerFunc {
 		return func(ctx context.Context, rw *amqprpc.ResponseWriter, d amqp.Delivery) {
